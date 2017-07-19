f0ff1392da66b64b51d0a37466e5aa51bc39bb80
Ben Hughes <benjamin-hughes@users.noreply.github.com>
2017-07-19 12:40:06 +0100

    Player 2 HP displayed

--- spec/features/names_spec.rb
+++ spec/features/names_spec.rb
@@ -6,4 +6,11 @@ feature 'fill in players name' do
     click_button 'Submit'
     expect(page).to have_content 'Antonio vs. Nandhini'
   end
+
+  scenario " Displays Player 2's hit points" do
+    visit('/')
+    fill_in :player_1_name, with: 'Antonio'
+    fill_in :player_2_name, with: 'Nandhini'
+    expect(page).to have_content 'Antonio vs. Nandhini HP: 100'
+  end
 end
