d73c05ad46f5357fa0c7e7cc8d54801b4b06721b
Ben Hughes <benjamin-hughes@users.noreply.github.com>
2017-07-19 15:20:00 +0100

    Player attacks completed

--- app.rb
+++ app.rb
@@ -15,18 +15,21 @@ class Battle < Sinatra::Base
   end
 
   get '/play' do
-    @player_1_name = session['player_1_name']
-    @player_2_name = session['player_2_name']
+    @player_1_name = session[:player_1_name]
+    @player_2_name = session[:player_2_name]
     @player_2_HP = 100
     session['player_2_HP'] = @player_2_HP
     erb :play
   end
 
   post '/attack' do
-    @player_2_HP = session['player_2_HP'] - 20
-    session['player_1_name'] = @player_1_name
-    session['player_2_name'] = @player_2_name
-    erb :play
+    # @player_2_HP = session['player_2_HP'] - 20
+    # session['player_1_name'] = @player_1_name
+    # session['player_2_name'] = @player_2_name
+    # erb :play
+    @player_1_name = session[:player_1_name]
+    @player_2_name = session[:player_2_name]
+    erb :attack
   end
 
   run! if app_file == $0
