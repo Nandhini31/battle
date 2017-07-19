ba5bab6e64161ad0360a8f6101171b8713beb6d0
nandhini <nandhini31@gmail.com>
2017-07-19 13:39:29 +0100

    attack

--- app.rb
+++ app.rb
@@ -18,9 +18,16 @@ class Battle < Sinatra::Base
     @player_1_name = session['player_1_name']
     @player_2_name = session['player_2_name']
     @player_2_HP = 100
+    session['player_2_HP'] = @player_2_HP
     erb :play
   end
 
+  post '/attack' do
+    @player_2_HP = session['player_2_HP'] - 20
+    session['player_1_name'] = @player_1_name
+    session['player_2_name'] = @player_2_name
+    erb :play
+  end
 
   run! if app_file == $0
 end
--- spec/features/names_spec.rb
+++ spec/features/names_spec.rb
@@ -1,11 +1,18 @@
 feature 'fill in players name' do
   scenario 'Can enter players name and see it on the screen' do
     sign_in_and_play
-    expect(page).to have_content 'Antonio vs. Nandhini'
+    expect(page).to have_content 'Antonio vs Nandhini'
   end
 
   scenario " Displays Player 2's hit points" do
     sign_in_and_play
-    expect(page).to have_content 'Antonio vs. Nandhini HP: 100'
+    expect(page).to have_content 'Antonio vs Nandhini HP: 100'
   end
+
+  scenario "Attack Player 2 and get confirmation" do
+    sign_in_and_play
+    click_button'attack'
+    expect(page).to have_content 'Antonio attacks Nandhini'
+  end
+
 end
