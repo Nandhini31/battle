a42932e30afc90d47df980de82a5fa72d44ea091
nandhini <nandhini31@gmail.com>
2017-07-20 11:15:56 +0100

    Skinny Controllers

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,12 +1,22 @@
 require 'game'
 
 describe Game do
-  subject(:game) {described_class.new}
+
+  let(:player1) {double :player1}
+  let(:player2) {double :player2}
+  subject(:game) {described_class.new(player1, player2)}
+
   let(:player){double :player, hurt: nil}
 
     describe '#attack' do
       it 'expects to call the hurt method' do
        expect {game.attack(player)}.not_to raise_error
      end
+    end
+
+    describe '#initialize' do
+      it 'expects to 2 arguments on instatiation' do
+        expect{described_class.new(player1,player2)}.not_to raise_error
+      end
     end
 end
