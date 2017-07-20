a2551661bc94daff891eab4e69cf1927e84e46de
oleglukyanov <oleg.s.lukyanov@gmail.com>
2017-07-20 12:31:53 +0100

    Small refactor

--- lib/game.rb
+++ lib/game.rb
@@ -2,9 +2,9 @@ class Game
   attr_reader :player_1, :player_2
 
   def initialize(player_1,player_2)
-    @player_1 = player_1
-    @player_2 = player_2
-    @players = [@player_1, @player_2]
+    @players = [player_1, player_2]
+    @player_1 = @players[0]
+    @player_2 = @players[1]
   end
 
   def attack(player)
